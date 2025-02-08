0d2f817309b3aa9e09f8248178d06dd5803bc9b6
Ajith-M-001 <ajithguru741@gmail.com>
2025-02-08 08:17:52 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+import connectDB from "./config/database.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to BLOG Application - build by Ajith");
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`server is running on the PORT http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("error connecting to database", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
